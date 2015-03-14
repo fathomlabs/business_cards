dc033e9788966d5968ef06136acca6cf33e0e5e9
Stephanie Unna <steph.unna@gmail.com>
2015-03-14 17:27:16 +0000

    Added option for number of cards to be generated.:

--- generate.sh
+++ generate.sh
@@ -4,7 +4,11 @@ simpleserver_PID=$!
 echo $simpleserver_PID
 sleep 10
 
-for i in `seq 1 10`;
+echo "How many business cards would you like to generate?"
+
+read ncards
+
+for i in `seq 1 $ncards`;
 do
   phantomjs screencapture.js
 done
